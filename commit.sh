20921cdb033c1916977c1975f41529626464b10a
Dmitry Barashev <dbarashev@bardsoftware.com>
2020-10-29 17:25:00 +0300

    removed clone+cd from the script

--- commit.sh
+++ commit.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
 PRJ=project$1
-git clone https://github.com/dbms-class/csc-2020-team-all
-cd csc-2020-team-all
 git checkout -b $PRJ
 mkdir $PRJ
 cd $PRJ
 cp "$2" $PRJ.sql
 git add $PRJ.sql
 git commit 
-git push origin
+git push origin $PRJ
